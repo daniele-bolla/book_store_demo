d1d9a036cb14489cabb9f2e918940e6c69c1ec73
bolladaniele <bolladaniele@gmail.com>
2020-09-24 14:30:15 +0100

    init module test

--- src/store/modules/books.ts
+++ src/store/modules/books.ts
@@ -3,26 +3,18 @@ import { VuexModule, Module, Mutation, Action } from "vuex-module-decorators";
 import { BookInterface } from "@/interfaces/BookInterface";
 import BookService from "@/services/BookService";
 
-@Module({ namespaced: true })
+@Module({ namespaced: true, name: "books" })
 class Books extends VuexModule {
   public all: BookInterface[] = [];
-  public test: string | undefined;
 
   @Mutation
   public GET_ALL(all: BookInterface[]) {
     this.all = all;
-    debugger;
   }
-  public GET_TEST(test: string) {
-    this.test = test;
-  }
+
   @Action({ commit: "GET_ALL", rawError: true })
   public async getAll() {
     return await BookService.getAll();
-  }
-  @Action({ commit: "GET_ALL", rawError: true })
-  getTest() {
-    return "books";
   }
 }
 export default Books;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
 <template>
   <div class="home container page">
     <div class="top-books__title">Top Books Of All Time</div>
-    <div class="top-book__list">
-      <div class="top--book__card" v-for="book in all" :key="book.slug">
-        {{ book }}
+    <div class="top-books-list">
+      <div
+        class="top-books-list__item top-book-card"
+        v-for="book in all"
+        :key="book.slug"
+      >
+        <div class="top-book-card__description">
+          <h2 class="top-book-card__title">
+            {{ book.title }}
+            <small class="top-book-card__rating">{{ book.rating }}</small>
+          </h2>
+          <h3 class="top-book-card__author">{{ book.author }}</h3>
+          <div class="top-book-card__synopsis">{{ book.synopsis }}</div>
+        </div>
+        <div class="top-book-card__picture">
+          <img
+            class="top-book-card__cover"
+            :src="book.cover"
+            :alt="book.title"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -11,16 +29,29 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+
 import { namespace } from "vuex-class";
 const books = namespace("books");
+
 @Component
 export default class Home extends Vue {
   mounted() {
     this.getAll();
   }
+
   @books.State
   public all!: Array<object>;
+
   @books.Action
   public getAll!: () => void;
 }
 </script>
+
+<style lang="scss">
+.top-books-list__item:nth-child(even) {
+  background: $card-bg;
+}
+.top-book-card {
+  display: flex;
+}
+</style>
